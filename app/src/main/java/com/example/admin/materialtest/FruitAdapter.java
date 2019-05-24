12d9cc66a6f3dc5ecffd0786fd379f1dffc817e4
YuQiao0303 <691761071@qq.com>
2019-05-24 17:31:14 +0800

    add fruit activity

--- app/src/main/java/com/example/admin/materialtest/FruitAdapter.java
+++ app/src/main/java/com/example/admin/materialtest/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.admin.materialtest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -34,9 +35,22 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (mContext == null) {
             mContext = parent.getContext();
-        } View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,
-                parent, false);
-        return new ViewHolder(view);
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, parent, false);
+
+        //水果点击事件
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+        return holder;
     }
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
